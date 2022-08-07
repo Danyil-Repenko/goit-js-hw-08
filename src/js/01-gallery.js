5b27e0628a22268bc64425436940e54cce6bf2e5
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-07 17:41:42 +0300

    завдання 1 та 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function galleryItemsMarkup(arrayOfItems) {
+    return arrayOfItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+}
+
+const insertGalleryItems = arrayOfItems => {
+    const markup = galleryItemsMarkup(arrayOfItems);
+    gallery.innerHTML = markup;
+}
+
+insertGalleryItems(galleryItems);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const ITEM_IN_LOCAL_STORAGE = 'videoplayer-current-time';
+
+function onVideoPlaying(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem(ITEM_IN_LOCAL_STORAGE, JSON.stringify(currentTime));
+}
+
+player.on('timeupdate', throttle(onVideoPlaying, 1000));
+
+function checkItem(timeStamp) {
+    if (timeStamp) {
+        const timeToSet = localStorage.getItem(timeStamp);
+        return JSON.parse(timeToSet);
+    }
+
+    return 0;
+}
+
+player.setCurrentTime(checkItem(ITEM_IN_LOCAL_STORAGE));
