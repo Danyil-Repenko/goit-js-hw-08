580ca254bc76020ca47c059053ecbf474cc8b286
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-07 21:07:00 +0300

    завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const ITEM_KEY = 'feedback-form-state';
+let formInputs = {};
+
+if (localStorage.getItem(ITEM_KEY)) {
+    const inputValues = JSON.parse(localStorage.getItem(ITEM_KEY));
+    formInputs = inputValues;
+    Object.keys(inputValues).forEach(key => {
+        form[key].value = inputValues[key];
+    });
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    formInputs[event.target.name] = event.target.value;
+    localStorage.setItem(ITEM_KEY, JSON.stringify(formInputs));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(ITEM_KEY)));
+    localStorage.removeItem(ITEM_KEY);
+    event.currentTarget.reset();
+}
